2ce6235011099a42927821d8262abe32ec54b106
Giap <ryon@conjurers.dev>
2023-01-12 17:26:39 +0800

    - Added MapView and MapAnnotation with applogo

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,13 +6,47 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    //MARK: - PROPERTIES
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    //MARK: - BODY
     var body: some View {
-        Text("Map")
+        //MARK: 1. BASIC MAP
+        //Map(coordinateRegion: $region)
+        
+        //MARK: 2. MAP WITH ANNOTATIONS
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            //PIN
+            //MapPin(coordinate: item.location, tint: .accentColor)
+            
+            //MARKER from SwiftUI
+            //MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            //CUSTOM ANNOTATION with AppLogo
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }//: MapAnnotation
+        })
     }
 }
 
+
+//MARK: - PREVIEW
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
