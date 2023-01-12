3f23cc12189a42e82ab3dada2f34192c51087780
Giap <ryon@conjurers.dev>
2023-01-12 14:19:51 +0800

    - Initial setups

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Giap on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
